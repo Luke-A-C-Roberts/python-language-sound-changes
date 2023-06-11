43c9dc8972ffb18e4729c126a94a7b483fc13bd7
Luke-A-C-Roberts <lrobertt44@gmail.com>
2023-06-11 19:02:25 +0000

    replancement sound change detects whether a word has the correct context

--- Program/__main__.py
+++ Program/__main__.py
@@ -1,4 +1,4 @@
-from re import match, fullmatch
+from re import search, fullmatch, Match
 
 class Catagory:
     def __init__(self, input_str: str) -> None:
@@ -10,12 +10,11 @@ class Catagory:
     def get_character_catagory(self) -> str:
         return "[" + self.characters + "]"
 
-    def __eq__(self, key: str | 'Catagory') -> bool:
+    def __eq__(self, key: str) -> bool:
         return key == self.symbol \
             or key == self.characters \
             or key == self.symbol + "=" + self.characters \
             or key == "[" + self.characters + "]" \
-            or (key.symbol == self.symbol and key.characters == self.characters)
 
     def compare_length(self, other: 'Catagory') -> bool:
         return len(self.characters) == len(other.characters)
@@ -25,7 +24,7 @@ class Catagories:
         lines = input_lines.splitlines()
         self.catagories = [Catagory(line) for line in lines]
 
-    def __getitem__(self, catagory_key: str | Catagory) -> str | None:
+    def __getitem__(self, catagory_key: str) -> str | None:
         for catagory in self.catagories:
             if catagory == catagory_key:
                 return catagory.get_character_catagory()
@@ -42,18 +41,43 @@ class SoundChange:
         self.context = context
         self.nontexts = nontexts
 
+    def substitute_into_context(self, context: str, value: str) -> str:
+         return context.replace("_", value) if "_" in context else context
+
+    def substitute_catagories(self, context: str, catagories: Catagories) -> str:
+        completed = ""
+        for character in context:
+            catagory = catagories[character]
+            if catagory == None:
+                completed += character
+                continue
+            completed += catagory
+        return completed
+
 class ReplacementSC(SoundChange):
-    def __init__(self, input_val: str, output_val: str, context: str, nontexts: list[str]) -> None:
+    def __init__(self, input_val: str, output_val: str, context: str, nontexts: list[str] = []) -> None:
         super().__init__(context, nontexts)
-        self.input_val = input_val
+        self.input_val  = input_val
         self.output_val = output_val
 
-    def compare_word(self, word: str) -> bool:
-        pass
-
+    def compare_word(self, word: str, catagories: Catagories) -> Match | None:
+        context = super().substitute_into_context(self.context, self.input_val)
+        context = super().substitute_catagories(context, catagories)
+        context = context.replace("...", ".+")
+        context = context.replace("(", "").replace(")", "?")
+        return search(context, word)
 
 class SoundChanges:
     pass
 
 class InputWords:
     pass
+
+def main():
+    c = Catagories("V=aiueo")
+    r = ReplacementSC("i", "j", "V_V")
+    m = r.compare_word("kaiu", c)
+    print(m)
+
+if __name__ == "__main__":
+    main()
