3a823cb6591539d2a52475407ae8fea2f606a06a
Luke-A-C-Roberts <lrobertt44@gmail.com>
2023-06-13 01:33:22 +0000

    added a filter for nontexts. it is wrong however when dealing with more than one underscore. for example if i have the SC i/j/V_V_C/_o and use the word kaioit to test, i expect it to detect the aio as a nontext but allow the context oit. at the moment doesnt understand the difference so just accepts kaioit.

--- Program/__main__.py
+++ Program/__main__.py
@@ -1,5 +1,8 @@
-from re import search, fullmatch, Match
+from re        import Match, Pattern, fullmatch, compile, finditer
+from functools import reduce
+from operator  import iconcat
 
+# a catagory holds characters together nealy, represented by a capital letter
 class Catagory:
     def __init__(self, input_str: str) -> None:
         if fullmatch("[A-Z]=.+", input_str) == None:
@@ -10,20 +13,24 @@ class Catagory:
     def get_character_catagory(self) -> str:
         return "[" + self.characters + "]"
 
+    # multiple ways for a Catagory to be equal
     def __eq__(self, key: str) -> bool:
         return key == self.symbol \
             or key == self.characters \
             or key == self.symbol + "=" + self.characters \
             or key == "[" + self.characters + "]" \
 
+    # used for special cases where a catagory is both the input and output of a sound change
     def compare_length(self, other: 'Catagory') -> bool:
         return len(self.characters) == len(other.characters)
 
+# holds catagories neatly together
 class Catagories:
     def __init__(self, input_lines: str) -> None:
         lines = input_lines.splitlines()
         self.catagories = [Catagory(line) for line in lines]
 
+    # a single catagory can be retrieved based on the key capital letter inputted
     def __getitem__(self, catagory_key: str) -> str | None:
         for catagory in self.catagories:
             if catagory == catagory_key:
@@ -36,17 +43,41 @@ class Catagories:
 #   3) Deletion
 #   4) Metathesis
 
+# sound change object used both for detecting contexts where a sound change can occur, and applying sound changes
 class SoundChange:
-    def __init__(self, context: str, nontexts: list[str]) -> None:
-        self.context = context
-        self.nontexts = nontexts
+    def __init__(self, catagories: Catagory, input_val: str, output_val: str, context: str, nontexts: list[str]) -> None:
+        self.input_val  = input_val
+        self.output_val = output_val
+        self.context    = context
+        self.nontexts   = nontexts
+
+        self.context_undescore_positions = [
+            index for index, character in enumerate(context) if character == "_"
+        ]
+        self.nontext_underscore_positions = [[
+                index for index, character in enumerate(nontext) if character == "_"
+            ] for nontext in nontexts
+        ]
+
+        self.context_pattern  = self.compile_context_pattern(context, catagories)
+        self.nontext_patterns = [self.compile_context_pattern(nontext, catagories) for nontext in nontexts]
+
+    def remove_higher_level_brackets(self, context: str) -> str:
+        completed = ""
+        current_level = 0
+        for character in list(context):
+            if character == "[": current_level += 1
+            if character == "]": current_level -= 1
+            if not(character in "[]" and current_level > 1):
+                completed += character
+        return completed
 
     def substitute_into_context(self, context: str, value: str) -> str:
-         return context.replace("_", value) if "_" in context else context
+         return context.replace("_", value) if "_" in context else context 
 
     def substitute_catagories(self, context: str, catagories: Catagories) -> str:
         completed = ""
-        for character in context:
+        for character in list(context):
             catagory = catagories[character]
             if catagory == None:
                 completed += character
@@ -54,18 +85,68 @@ class SoundChange:
             completed += catagory
         return completed
 
-class ReplacementSC(SoundChange):
-    def __init__(self, input_val: str, output_val: str, context: str, nontexts: list[str] = []) -> None:
-        super().__init__(context, nontexts)
-        self.input_val  = input_val
-        self.output_val = output_val
+    def substitute_ellipses(self, context: str) -> str:
+        return context.replace("...", ".+")
 
-    def compare_word(self, word: str, catagories: Catagories) -> Match | None:
-        context = super().substitute_into_context(self.context, self.input_val)
-        context = super().substitute_catagories(context, catagories)
-        context = context.replace("...", ".+")
-        context = context.replace("(", "").replace(")", "?")
-        return search(context, word)
+    def substitute_curlies(self, context: str) -> str:
+        return context.replace("(", "[").replace(")", "]?")
+
+    def replace_squares(self, context: str) -> str:
+        completed = ""
+        for character in list(context):
+            completed += "{2}" if character == "²" else character
+        return completed
+
+    def compile_context_pattern(self, context: str, catagories: Catagories) -> Pattern: 
+        context = self.substitute_into_context(context, self.input_val) # _ -> input_val
+        context = self.substitute_curlies(context)                      # (X) -> [X]?
+        context = self.substitute_catagories(context, catagories)       # X -> [xyz]
+        context = self.substitute_ellipses(context)                     # ... -> .+
+        context = self.remove_higher_level_brackets(context)            # [#[xyz]] -> [#xyz]
+        context = self.replace_squares(context)                         # x² -> x{2}
+
+        return compile(context)
+
+    def filter_context_matches(self, context_matches: list[Match], nontext_matches: list[tuple[Match, str]]) -> list[Match]:
+        valid_context_matches  : list[Match] = []
+        context_input_positions: list[int]   = []
+        nontext_input_positions: list[int]   = []
+
+        for context_match in context_matches:
+            for underscore_position in self.context_undescore_positions:
+                context_input_positions.append(underscore_position + context_match.span()[0])
+                valid_context_matches.append(context_match)
+
+        for nontext_match in nontext_matches:
+            this_pattern_str = nontext_match[1]
+            pattern_index = -1
+            for index, nontext_pattern in enumerate(self.nontext_patterns):
+                if this_pattern_str == nontext_pattern.pattern:
+                    pattern_index = index
+                    break
+
+            nontext_input_positions += [
+                underscore_position + nontext_match[0].span()[0]
+                for underscore_position in self.nontext_underscore_positions[pattern_index] # which patten?
+            ]
+
+        valid_input_positions = list(set(context_input_positions).difference(set(nontext_input_positions)))
+        return list(set([valid_context_matches[context_input_positions.index(index)] for index in valid_input_positions]))
+
+    def compare_word(self, word: str) -> list[Match]:
+        context_matches = [
+            context_match for context_match in finditer(self.context_pattern, f"#{word}#")
+        ]
+        nontext_matches = reduce(iconcat, [[
+                (nontext_match, nontext_pattern.pattern) for nontext_match in finditer(nontext_pattern, f"#{word}#")
+            ] for nontext_pattern in self.nontext_patterns
+        ], [])
+        
+        return self.filter_context_matches(context_matches, nontext_matches)
+
+class ReplacementSC(SoundChange):
+    def __init__(self, catagories: Catagories, input_val: str, output_val: str, context: str, nontexts: list[str] = []) -> None:
+        super().__init__(catagories, input_val, output_val, context, nontexts)
 
 class SoundChanges:
     pass
@@ -74,10 +155,19 @@ class InputWords:
     pass
 
 def main():
-    c = Catagories("V=aiueo")
-    r = ReplacementSC("i", "j", "V_V")
-    m = r.compare_word("kaiu", c)
-    print(m)
+    c = Catagories("V=aiueo\nC=ptkbdghmnŋslr")
+    r = SoundChange (
+        c,
+        input("expected input: "),
+        input("expected output: "),
+        input("sound change context: "),
+        [input("sound change nontext: ")]
+    )
+    ws = input("word(s)").split(" ")
+    ms = [r.compare_word(w) for w in ws]
+
+    for i in range(len(ws)):
+        print(ws[i] + " " + str(ms[i]))
 
 if __name__ == "__main__":
     main()
